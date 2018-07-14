373c2536d194b33bcf09b8512a0de2c258f5539a
Teo <teochaban@gmail.com>
2018-07-13 21:50:48 -0300

    Added Java template

---
+++ templateJava.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+public class Icpc {
+
+	public static void main(String... args) throws Exception {
+		FastScan sc = new FastScan(new BufferedReader(new InputStreamReader(System.in)));
+		PrintWriter pw = new PrintWriter(System.out);
+
+		
+
+		sc.close();
+		pw.close();
+		System.exit(0);
+	}
+
+	static class FastScan implements Closeable {
+		private BufferedReader br;
+		private StringTokenizer tk;
+
+		public FastScan(BufferedReader br) {
+			this.br = br;
+		}
+
+		public int in() throws NumberFormatException, IOException {
+			return Integer.parseInt(next());
+		}
+
+		public long ln() throws NumberFormatException, IOException {
+			return Long.parseLong(next());
+		}
+
+		public double db() throws NumberFormatException, IOException {
+			return Double.parseDouble(next());
+		}
+
+		@Override
+		public void close() throws IOException {
+			tk = null;
+			br.close();
+		}
+
+		public String next() throws IOException {
+			while (tk == null || !tk.hasMoreTokens()) {
+				String line = br.readLine();
+				if (line == null)
+					return null;
+				tk = new StringTokenizer(line);
+			}
+			return tk.nextToken();
+		}
+	}
+}
