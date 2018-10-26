0a987c09623e8395a51a8e4160437d3274315300
Teo Magnino Chaban <teochaban@mail.utoronto.ca>
2018-10-26 15:08:34 -0400

    matrix power

---
+++ Algorithms/matrixpower.cpp
@@ -0,0 +1,35 @@
+typedef long long ll;
+const int NMAT=2;
+const int mod=1;
+
+/* c=a*b */
+void mu(ll a[][NMAT], ll b[][NMAT], ll c[][NMAT], int _n) {
+	for(int i=0;i<_n;i++)
+		for(int j=0;j<_n;j++) {
+			c[i][j]=0;
+			for(int h=0;h<_n;h++) {
+				c[i][j]+=(a[i][h]*b[h][j])%mod;
+				c[i][j]%=mod;
+			}
+		}
+}
+/*returns ans=mat^b*/
+void power(ll ans[][NMAT], ll mat[][NMAT], ll b, int _n) {
+	ll tmp[NMAT][NMAT];
+	for(int i=0;i<_n;i++)
+		for (int j = 0; j<_n; j++)
+			ans[i][j]=i==j;
+	while(b) {
+		if(b&1) {
+			mu(ans, mat, tmp, _n);
+			for(int i=0;i<_n;i++)
+				for(int j=0;j<_n;j++)
+					ans[i][j]=tmp[i][j];
+		}
+		mu(mat, mat, tmp, _n);
+		for(int i=0;i<_n;i++)
+			for(int j=0;j<_n;j++)
+				mat[i][j]=tmp[i][j];
+		b>>=1;
+	}
+}
